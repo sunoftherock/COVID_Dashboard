05c04598714e135d20003903ecded4a819e48a45
Rocky <me.rocky.sun@gmail.com>
2021-02-13 17:54:09 -0500

    uploaded scss

--- scripts/htmlUpdater.js
+++ scripts/htmlUpdater.js
@@ -1,45 +1,39 @@
 var document = window.document;
 
-var hi;
 
 // const api_url = 'https://api.covid19tracker.ca/summary';
 const api_url = 'https://api.apify.com/v2/key-value-stores/fabbocwKrtxSDf96h/records/LATEST?disableRedirect=true';
-async function getDATA() {
-const response = await fetch(api_url);
-const data = await response.json();
-response.then(hi => data.infected, console.log(':(')).catch(console.log(':\'('));
-console.log(data.infected);
+var province = 'Ontario';
 
-return toString(data.infected);
+// Total cases Canada
+async function getDATA() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log(data.infected);
+    if (province == 'Ontario') {
+      const {Ontario, infectedCount} = data;
+      document.getElementById('main title').textContent = `There have been a total of ${region} cases in Ontario`;
+    }
+    
+   //  const {infected} = data;
+   //  document.getElementById('main title').textContent = `There have been a total of ${infected} cases in Canada`;
 }
-//temp = getDATA();
+
+temp = getDATA();
 
 var result;
- 
-
-fetch('https://api.apify.com/v2/key-value-stores/fabbocwKrtxSDf96h/records/LATEST?disableRedirect=true', {})
-    .then(response => response.json());
-    .then(data => console.log(data))
-
-//var chance = total_cases.GET('https://api.covid19tracker.ca/summary');
-
-//var chance = 10;
-
 
 var paragraphElement = document.createElement('h1');
 //creates <h1></h1>
 
 
-paragraphElement.innerHTML = hi //`There's a ${temp}% chance you will die if you go out`
+// paragraphElement.innerHTML = `There's a ${temp}% chance you will die if you go out`
 //creates <h1>`there's a ${chance}% chance you will die if you go out`</h1>
 
 
-var element = document.getElementById('main title');
+//var element = document.getElementById('Fact2');
 //looks for the 'main title' tag inside the document
 
 
 element.appendChild(paragraphElement);
 //<div id= "main title"><h1>`there's a ${chance}% chance you will die if you go out`</h1></div>
-
-
-
